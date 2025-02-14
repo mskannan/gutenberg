0e544a6cd819cf5274f70842a6c7c2fa186680b1
mskannan <senthkannan55@gmail.com>
2025-02-15 00:48:33 +0530

    Category Logic

--- books/views.py
+++ books/views.py
@@ -1,4 +1,42 @@
 from django.shortcuts import render,redirect
+import requests 
 
 def index(request):
     return render(request,'index.html')
+
+
+def fetchapi(generename): 
+
+    url = f"http://skunkworks.ignitesol.com:8000/books/?topic={generename}"
+
+    # Set headers to accept JSON response
+    headers = {
+        "Accept": "application/json"
+    }
+
+    # Make the GET request
+    response = requests.get(url, headers=headers)
+
+    # Check if the response was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data 
+
+
+def category(request,generename):
+    capitalized_generename = generename.capitalize() 
+    apidata = fetchapi(generename) 
+    apidataresult = apidata['results']
+    cachelist = []
+    for data in apidataresult:
+        imgval = data['formats']
+        fulltitle = data['title']
+        cachelist.append({
+            'title': ' '.join(fulltitle.split(" ")[:5]),
+            'fulltitle':fulltitle,
+            'authors': data['authors'],
+            "img": imgval.get('image/jpeg')
+        })
+    context = {'bookdata': cachelist,"capitalized_generename":capitalized_generename}
+    return render(request,'category.html',context)
--- gutenberg/urls.py
+++ gutenberg/urls.py
@@ -20,5 +20,6 @@ from books import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('category/<str:generename>',views.category,name="category"),
 ]
